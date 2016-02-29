357d65ed2bf4b2d0ae68f95ef75115342acec00e
SIS-hackathon <dfurtado@socialinterest.org>
2016-02-29 10:45:45 -0800

    Update slphack.py

--- slphack.py
+++ slphack.py
@@ -1,6 +1,6 @@
 #
 # SLPHACK's slphack.py
-# Copyright (c) 2016 The Center to Promote Healtcare Access, Inc., DBA Social Interest Solutions
+# Copyright (c) 2016 The Center to Promote Healthcare Access, Inc., DBA Social Interest Solutions
 # Licensed under MIT (https://github.com/SIS-hackathon/slphack/LICENSE)
 #
 
